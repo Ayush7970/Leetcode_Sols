87361d05cedf9f770d48bf64c27abe0b6e0795e6
Ayush Bhardwaj <ayush975600@gmail.com>
2026-05-12 00:05:01 -0500

    leetcode: update 55_jump_game.py

--- TopInterview150/55_jump_game.py
+++ TopInterview150/55_jump_game.py
@@ -2,25 +2,55 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
         """
-        Try Again
-        Greedy Problem: I tried with dfs and memoized dfs both of them did not passed for time complexity. The trick was greedy. You simply ahve to loop form the back and check if the i + nums[i] is reachable to the goal then i is the new goal. Simple greedy, breaking down the problem from the back. LEARN THIS TECHNIQUE.
+        I cracked it this time. This was a greedy questions and I used the approach of traversing from the back. 
+        T.C -> O(n)
+        S.C -> O(1)
         """
 
-        goal = len(nums) - 1
+        location = len(nums)
 
         for i in range(len(nums) - 1, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
+            if i + 1 + nums[i] >= location:
+                location = i + 1
+            
+        return True if location == 1 else False
+
+        # practice 1 Greedy, identfied start from back
+        # goal = len(nums) - 1
+
+        # for i in range(len(nums) - 1, -1, -1):
+
+        #     if i + nums[i] >= goal:
+        #         goal = i
         
-        return True if goal == 0 else False
+        # return True if not goal else False
 
 
+
+
+
+
+
+    #     """
+    #     Try Again
+    #     Greedy Problem: I tried with dfs and memoized dfs both of them did not passed for time complexity. The trick was greedy. You simply ahve to loop form the back and check if the i + nums[i] is reachable to the goal then i is the new goal. Simple greedy, breaking down the problem from the back. LEARN THIS TECHNIQUE.
+    #     """
 
     #     goal = len(nums) - 1
 
-    #     for i in range(len(nums)- 1, -1, -1):
-    #         if nums[i] + i >= goal:
+    #     for i in range(len(nums) - 1, -1, -1):
+    #         if i + nums[i] >= goal:
     #             goal = i
+        
     #     return True if goal == 0 else False
+
+
+
+    # #     goal = len(nums) - 1
+
+    # #     for i in range(len(nums)- 1, -1, -1):
+    # #         if nums[i] + i >= goal:
+    # #             goal = i
+    # #     return True if goal == 0 else False
 
         
